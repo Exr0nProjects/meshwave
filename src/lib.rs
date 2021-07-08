c7a7180d032240e5e592ad337ed220f906929268
Exr0n <mail@exr0n.com>
2021-07-08 15:00:45 -0700

    needed begin_path for actual lines

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,13 @@ use rand::distributions::{ Distribution, uniform::{ Uniform, UniformFloat } };
 use std::iter::repeat;
 use std::mem;
 
-const UPDATE_RATE: u32 = 12; // updates per second
+const UPDATE_RATE: u32 = 30; // updates per second
 
-const NUM_POINTS: usize = 6; 
+const NUM_POINTS: usize = 12; 
 const NOISE_RANGE: f64 = 100.;
 const NOISE_SCALE: f64 = 300.;
-const RESOLUTION: f64 = 0.05; // points per pixel
+const CHANGE_SPEED: f64 = 0.1;
+const RESOLUTION: f64 = 0.1; // points per pixel
 
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
@@ -59,8 +60,6 @@ struct Lines {
 }
 impl Lines {
     fn new(canvas: web_sys::HtmlCanvasElement, points: usize) -> Lines {
-    //fn new(canvas: web_sys::HtmlCanvasElement, points: usize) {
-    //fn new(canvas: web_sys::HtmlCanvasElement, ctx: web_sys::CanvasRenderingContext2d, points: usize) -> Lines {
         let ctx = canvas
             .get_context("2d")
             .unwrap()
@@ -71,7 +70,6 @@ impl Lines {
         ctx.set_fill_style(  &JsValue::from_str(&format!("green")));
 
         let (size_w, size_h) = (canvas.client_width() as f64, canvas.client_height() as f64);
-        //canvas.set_height(size_h as u32);
         
         let mut rng = thread_rng();
         let udist_w = Uniform::new(0., size_w);
@@ -80,10 +78,6 @@ impl Lines {
         let points = repeat(()).take(points)
             .map(|_| (udist_w.sample(&mut rng), udist_h.sample(&mut rng)))
             .collect::<Vec<(f64, f64)>>();
-
-        //for point in &points {
-        //    console::log_1(&JsValue::from_str(&format!("point at {}, {}", point.0, point.1)));
-        //}
 
         let mut lines = Vec::<Line>::new();
         lines.reserve(points.len().pow(2));
@@ -101,6 +95,7 @@ impl Lines {
     fn draw_line(&self, line: &Line, pos: f64) {
         let f = |x: f64| (line.0.1-line.1.1) / (line.0.0-line.1.0) * (x - line.0.0) + line.0.1;
 
+        self.ctx.begin_path();
         self.ctx.move_to(line.0.0, line.0.1);
         let dist = ((line.1.0 - line.0.0).powf(2.)
                    +(line.1.1 - line.0.1).powf(2.)).sqrt();
@@ -108,26 +103,21 @@ impl Lines {
         for i in 0..num {
             let x = line.0.0 + (i as f64/num as f64) * (line.1.0 - line.0.0);
             let y = f(x);
-            //console::log_1(&JsValue::from_str(&format!("line {}..{}: {}", line.0.0, line.1.0, x)));
 
-            let noise_x = self.noise.get([x/NOISE_SCALE, y/NOISE_SCALE,  pos]) as f64 * NOISE_RANGE;
-            let noise_y = self.noise.get([x/NOISE_SCALE, y/NOISE_SCALE, -pos]) as f64 * NOISE_RANGE;
+            let noise_x = self.noise.get([x/NOISE_SCALE, y/NOISE_SCALE,  pos*CHANGE_SPEED]) as f64 * NOISE_RANGE;
+            let noise_y = self.noise.get([x/NOISE_SCALE, y/NOISE_SCALE, -pos*CHANGE_SPEED]) as f64 * NOISE_RANGE;
             //let noise_x = 0.;
             //let noise_y = 0.;
 
             self.ctx.line_to(x + noise_x, y + noise_y);
         }
-        //for x_int in 0..((line.1.0 - line.0.0) as f64 *RESOLUTION) as i32 {
-        //    let x = line.0.0 + x_int as f64 / RESOLUTION;
-        //    self.ctx.line_to(line.0.0 + x, line.0.1 + f(x)); // TODO: add noise
-        //}
         self.ctx.line_to(line.1.0, line.1.1);
         self.ctx.stroke();
         //console::log_1(&JsValue::from_str(&format!("\n")));
     }
     fn render(&self, pos: f64) {
         self.ctx.fill_rect(0., 0., self.size_w-10., self.size_h-10.);
-        //self.ctx.clear_rect(0., 0., self.size_w, self.size_h);
+        self.ctx.clear_rect(0., 0., self.size_w, self.size_h);
         for line in &self.lines {
             self.draw_line(line, pos);
         }
@@ -135,15 +125,6 @@ impl Lines {
         for point in &self.points {
             self.ctx.fill_rect(point.0 - 5., point.1 - 5., 10., 10.);
         }
-        //console::log_1(&JsValue::from_str(&format!("herro after {}", pos)));
-        //let window = web_sys::window().unwrap();
-        //let document = window.document().unwrap();
-        //let canvas = document.get_element_by_id("canvas").unwrap();
-        //let canvas: web_sys::HtmlCanvasElement = canvas
-        //    .dyn_into::<web_sys::HtmlCanvasElement>()
-        //    .map_err(|_| ())
-        //    .unwrap();
-        //console::log_1(&JsValue::from_str(&format!("client size: {}, {}", canvas.width(), canvas.height())));
     }
 }
 
