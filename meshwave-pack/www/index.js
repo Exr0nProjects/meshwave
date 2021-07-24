557fab65f78ec4933bef21da2c07920c35f15a36
Exr0n <mail@exr0n.com>
2021-07-24 16:40:57 -0700

    actually use once event properly

--- meshwave-pack/www/index.js
+++ meshwave-pack/www/index.js
@@ -1,14 +1,6 @@
 import * as wasm from "meshwave";
 
-var enabled = false;
-
-window.addEventListener('load', () => {
-    document.getElementById("enable-meshwave").addEventListener('click', (e) => {
-        e.target.style = "color: #ccc !important;";
-        console.log(enabled)
-        if (!enabled) {
-            enabled = true;
-            wasm.greet();
-        }
-    });
-});
+document.getElementById("enable-meshwave").addEventListener('click', (e) => {
+    e.target.style = "color: #ccc !important;";
+    wasm.greet();
+}, { once: true });
